a3aa139e085d0d27ad4d11acdd0a6f84a2f2b4d4
SyafiqShidiq <isseiarif@gmail.com>
2023-12-13 20:07:42 +0700

    tambahport

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ const menulist = document.getElementById("menu-list");
 menuicon.addEventListener("click", () =>{
   menulist.classList.toggle("hidden");
 })
+
+function pengrajin() {
+  window.open("https://pengrajinkuningan.com/,", "_blank");
+}
+function template() {
+  window.open("https://pengrajinkuningan.com/sekolah/", "_blank");
+}
+function portkedua(){
+  window.open("https://syafiqshidiq.github.io/syafiq.github.io/", "_blank")
+}
+function osis(){
+  window.open("https://osismatiq.com/", "_blank");
+}
